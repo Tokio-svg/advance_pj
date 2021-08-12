47b3113655e81d4d576fcacb64ada2d4cbad6e84
TOKIO <tokkipt935@gmail.com>
2021-08-12 21:45:56 +0900

    エラーメッセージ表示修正

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,24 +10,19 @@
 <main class="shadow">
   <h1 class="card_title">Login</h1>
   <div class="form_wrap">
+    <p id="error_auth" class="error" style="display: none;">メールアドレスまたはパスワードが違います</p>
     <form method="POST" action="/login">
       @csrf
       <!-- メールアドレス -->
       <div>
         <img src="{{putSource('/img/mail.png')}}" alt="no image">
         <input id="email" type="email" name="email" value="{{old('email')}}" placeholder="Email" onblur="validateRequire(this.id,'error_email-require')" required />
-        @error('email')
-        <p class="error">{{$message}}</p>
-        @enderror
         <p id="error_email-require" class="error" style="display: none;">メールアドレスを入力してください</p>
       </div>
       <!-- パスワード -->
       <div>
         <img src="{{putSource('/img/key.png')}}" alt="no image">
         <input id="password" type="password" name="password" placeholder="Password" onblur="validateRequire(this.id,'error_password-require')" required />
-        @error('password')
-        <p class="error">{{$message}}</p>
-        @enderror
         <p id="error_password-require" class="error" style="display: none;">パスワードを入力してください</p>
       </div>
       <button type="submit">ログイン</button>
@@ -38,13 +33,45 @@
 
 @section('script')
 <script>
+  // 読み込み時にlaravelエラーメッセージの有無を取得して
+  // 対応するエラーメッセージを表示状態に変更する
+  window.onload = function() {
+    let errors = {
+      email: [],
+      password: [],
+    };
+    // 1.$errorから全エラーメッセージを取得する
+    <?php
+    if ($errors->has('email')) {
+      $tmp = $errors->first('email');
+      echo "errors.email.push('{$tmp}');";
+    }
+    if ($errors->has('password')) {
+      $tmp = $errors->get('password');
+      foreach ($tmp as $error) {
+        echo "errors.password.push('{$error}');";
+      }
+    }
+
+    ?>
+    // 2.各エラーメッセージごとに表示するかどうかチェックする
+    if (errors.email[0]) {
+      document.getElementById('error_email-require').style.display = "block";
+    }
+    errors.password.forEach((e) => {
+      if (e === 'パスワードを入力してください') {
+        document.getElementById('error_password-require').style.display = "block";
+      }
+      if (e === 'メールアドレスまたはパスワードが違います') {
+        document.getElementById('error_auth').style.display = "block";
+      }
+    })
+
+  }
+
   // 関数：入力必須バリデーション
   function validateRequire(id, errorId) {
     const errorMessage = document.getElementById(errorId);
-    // // 該当のエラーメッセージ要素が存在しない場合は終了
-    // if (!errorMessage) {
-    //   return;
-    // }
     const input = document.getElementById(id).value;
     if (!input) {
       errorMessage.style.display = "block";
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -16,28 +16,20 @@
       <div>
         <img src="{{putSource('/img/person.png')}}" alt="no image">
         <input id="name" type="text" name="name" value="{{old('name')}}" placeholder="Username" onblur="validateRequire(this.id,'error_name-require')" required />
-        @error('name')
-        <p class="error">{{$message}}</p>
-        @enderror
         <p id="error_name-require" class="error" style="display: none;">名前を入力してください</p>
       </div>
       <!-- メールアドレス -->
       <div>
         <img src="{{putSource('/img/mail.png')}}" alt="no image">
         <input id="email" type="email" name="email" value="{{old('email')}}" placeholder="Email" onblur="validateRequire(this.id,'error_email-require'); validateEmail()" required />
-        @error('email')
-        <p class="error">{{$message}}</p>
-        @enderror
         <p id="error_email-require" class="error" style="display: none;">メールアドレスを入力してください</p>
         <p id="error_email-type" class="error" style="display: none;">メールアドレスの形式で入力してください</p>
+        <p id="error_email-unique" class="error" style="display: none;">そのメールアドレスは既に使用されています</p>
       </div>
       <!-- パスワード -->
       <div>
         <img src="{{putSource('/img/key.png')}}" alt="no image">
         <input id="password" type="password" name="password" placeholder="Password" onblur="validatePasswordMin()" required />
-        @error('password')
-        <p class="error">{{$message}}</p>
-        @enderror
         <p id="error_password-min" class="error" style="display: none;">パスワードは8文字以上で入力してください</p>
       </div>
       <button type="submit">登録</button>
@@ -48,13 +40,54 @@
 
 @section('script')
 <script>
+  // 読み込み時にlaravelエラーメッセージの有無を取得して
+  // 対応するエラーメッセージを表示状態に変更する
+  window.onload = function() {
+    let errors = {
+      name: [],
+      email: [],
+      password: [],
+    };
+    // 1.$errorから全エラーメッセージを取得する
+    <?php
+    if ($errors->has('name')) {
+      $tmp = $errors->first('name');
+      echo "errors.name.push('{$tmp}');";
+    }
+    if ($errors->has('email')) {
+      $tmp = $errors->get('email');
+      foreach ($tmp as $error) {
+        echo "errors.email.push('{$error}');";
+      }
+    }
+    if ($errors->has('password')) {
+      $tmp = $errors->first('password');
+      echo "errors.password.push('{$tmp}');";
+    }
+    ?>
+    // 2.各エラーメッセージごとに表示するかどうかチェックする
+    if (errors.name[0]) {
+      document.getElementById('error_name-require').style.display = "block";
+    }
+    errors.email.forEach((e) => {
+      if (e === 'メールアドレスを入力してください') {
+        document.getElementById('error_email-require').style.display = "block";
+      }
+      if (e === 'メールアドレスの形式で入力してください') {
+        document.getElementById('error_email-type').style.display = "block";
+      }
+      if (e === 'そのメールアドレスは既に使用されています') {
+        document.getElementById('error_email-unique').style.display = "block";
+      }
+    })
+    if (errors.password[0]) {
+      document.getElementById('error_password-min').style.display = "block";
+    }
+  }
+
   // 関数：入力必須バリデーション
   function validateRequire(id, errorId) {
     const errorMessage = document.getElementById(errorId);
-    // // 該当のエラーメッセージ要素が存在しない場合は終了
-    // if (!errorMessage) {
-    //   return;
-    // }
     const input = document.getElementById(id).value;
     if (!input) {
       errorMessage.style.display = "block";
@@ -67,10 +100,6 @@
   // 1文字以上 @ 1文字以上 . 1文字以上の形の文字列をメールアドレス形式とする
   function validateEmail() {
     const errorMessage = document.getElementById('error_email-type');
-    // // 該当のエラーメッセージ要素が存在しない場合は終了
-    // if (!errorMessage) {
-    //   return;
-    // }
     const input = document.getElementById("email").value;
     // 未入力の場合はエラーメッセージを非表示にして終了
     if (!input) {
@@ -88,10 +117,6 @@
   // 関数：パスワード8文字以上バリデーション
   function validatePasswordMin() {
     const errorMessage = document.getElementById('error_password-min');
-    // // 該当のエラーメッセージ要素が存在しない場合は終了
-    // if (!errorMessage) {
-    //   return;
-    // }
     const input = document.getElementById('password');
     if (input.value.length < 8) {
       errorMessage.style.display = "block";
