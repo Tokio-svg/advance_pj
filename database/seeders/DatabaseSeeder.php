e0c0bce33607f1d070b6f412adf6f3a84af98913
TOKIO <tokkipt935@gmail.com>
2021-09-07 11:10:13 +0900

    ShopsTableSeeder.php 修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(ShopsTableSeeder::class);
+        $this->call(ShopsTableSeeder::class);
         // $this->call(AreasTableSeeder::class);
         // $this->call(GenresTableSeeder::class);
-        // $this->call(SchedulesTableSeeder::class);
         // $this->call(EvaluationsTableSeeder::class);
         // $this->call(UsersTableSeeder::class);
     }
